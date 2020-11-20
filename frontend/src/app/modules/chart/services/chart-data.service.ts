a979211f49f7af4a8960dff2935d57f075874db3
Elie NEHME <elie.nehme@soprasterianext.com>
2020-11-20 16:11:26 +0100

    🔥 Typo and update dependencies

--- frontend/src/app/modules/chart/services/chart-data.service.ts
+++ frontend/src/app/modules/chart/services/chart-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { take, tap, takeLast } from 'rxjs/operators';
+import { takeLast, tap } from 'rxjs/operators';
 
 import { CHART_URL, ChartAvailableOption } from '../interfaces/chart.interface';
 import { ChartStore } from '../store/chart.store';
@@ -10,19 +10,19 @@ export class ChartDataService {
   private availableCharts = ['bar', 'column', 'series'];
 
   constructor(
-    private store: ChartStore,
+    private chartStore: ChartStore,
     private http: HttpClient,
     @Inject(CHART_URL) private chartUrl: string
   ) {}
 
   apply(chart: ChartAvailableOption): void {
-    this.store.set('chartData', undefined);
+    this.chartStore.set('chartData', undefined);
 
     if (this.availableCharts.includes(chart)) {
       this.http
         .get(`${this.chartUrl}/${chart}`)
         .pipe(
-          tap(data => this.store.set('chartData', data)),
+          tap(data => this.chartStore.set('chartData', data)),
           takeLast(1)
         )
         .subscribe();
