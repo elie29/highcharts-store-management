53f4a0225a50bcdafbaa0a1db6bad18f60914f85
Elie NEHME <elie.nehme@soprasterianext.com>
2022-07-06 16:18:14 +0200

    🔥 Add deep clone confirguration example

--- frontend/src/app/modules/chart/store/chart.store.ts
+++ frontend/src/app/modules/chart/store/chart.store.ts
@@ -5,12 +5,15 @@ import { environment } from 'src/environments/environment';
 
 import { ChartState, INITIAL_CHART_STATE } from './chart.state';
 
+const JSONDeepClone = value => value && JSON.parse(JSON.stringify(value));
+
 // Injected in ChartModule Providers only
 @Injectable()
 export class ChartStore extends Store<ChartState> {
   constructor() {
     super(INITIAL_CHART_STATE, {
       cloneStrategy: identity, // data are not modified so no need to clone
+      // cloneStrategy: JSONDeepClone, // data are deep cloned, is it necessary in our use case??
       logChanges: environment.production === false
     });
   }
